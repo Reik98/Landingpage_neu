f5fa83b649963631b40a96a6ae708a09ba2742d4
Reik98 <reikba@yahoo.de>
2025-06-25 11:04:01 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -64,25 +64,7 @@ st.markdown("""
             background-color: #008B92;
             text-decoration: underline;
             }
-        .feature-box {
-    box-sizing: border-box;
-    overflow-wrap: break-word;
-    word-wrap: break-word;
-    width: 100%;
-}
-
-.feature-box ul {
-    padding-left: 1.2rem;
-    margin-top: 0.5rem;
-    margin-bottom: 0;
-    list-style-position: inside;  /* ✔ Bullet Points bleiben in der Box */
-}
-
-.feature-box li {
-    font-size: 1rem;
-    line-height: 1.4;
-    margin-bottom: 0.4rem;
-    overflow-wrap: break-word;
+    
 
     
         .feature-box {
@@ -189,6 +171,7 @@ st.markdown("""
     .feature-box ul {
         padding-left: 1rem;
         margin: 0;
+        list-style-position: inside;
     }
     .centered-heading {
         font-size: 1.2rem;
