1382fe408416831d2b6102b508c8bb92f832cf1e
Reik98 <reikba@yahoo.de>
2025-06-28 15:32:11 +0200

    Update Kulturdiagnostik_Integrationsstrategie.py

--- pages/Kulturdiagnostik_Integrationsstrategie.py
+++ pages/Kulturdiagnostik_Integrationsstrategie.py
@@ -30,7 +30,7 @@ st.markdown("""
     align-items: center;
     color: white;
     text-align: center;
-    text-shadow: 0 0 10px rgba(0,0,0,0.6);
+    text-shadow: 0 0 10px rgba(0,0,0,0.8);
 }
 
 /* ---------- TEXT ---------- */
