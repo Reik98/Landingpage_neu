713d2bae1e2d3ed996b5cad5efb782b0a7507c7b
Reik98 <reikba@yahoo.de>
2025-06-28 15:25:42 +0200

    Update Kulturdiagnostik_Integrationsstrategie.py

--- pages/Kulturdiagnostik_Integrationsstrategie.py
+++ pages/Kulturdiagnostik_Integrationsstrategie.py
@@ -20,7 +20,7 @@ st.markdown("""
     height: 700px;
     background-image: linear-gradient(rgba(0,0,0,0.4), rgba(0,0,0,0.4)),
                       url('https://raw.githubusercontent.com/Reik98/Landingpage/main/Kultur.jpg');
-    background-size: cover;
+    background-size: contain;
     background-repeat: no-repeat;
     background-position: center;
     display: flex;
